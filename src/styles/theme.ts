d4cb9e22273610eb1e912a8fe442e54a45aa924b
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-12-05 23:02:34 +0900

    feat: theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  mainBlueColor: '#1C7ED6',
+  uiBlueColor: '#1864AB',
+  uiReadColor: '#F03E3E',
+
+  blackContentColor: '#000000',
+  whiteContentColor: '#ffffff',
+
+  darkGray: '#5b5b5b',
+  dailyGray: '#808080',
+  ligthGray: '#f3f3f3',
+
+  sundayColor: '#D00B0B',
+  saturdayColor: '#0030AD',
+  weekdayCalendarColor: '#F6FBFF',
+  weekendCalendarColor: '#DBEFFF',
+};
