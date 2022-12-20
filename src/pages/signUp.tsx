ba335f4bcc05444999eccca0f508af242351b1ae
yoon-ho7 <suho0317@gmail.com>
2022-12-20 22:34:24 +0900

    [UPDATE] signup 퍼블리싱

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,34 +1,70 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
 
+interface SignUpProps {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
 const SignUp = (): JSX.Element => {
+  const [signupState, setSignupState] = useState<SignUpProps>({
+    email: '',
+    password: '',
+    nickname: '',
+  });
+
+  const { email, password, nickname } = signupState;
+
+  const SignUpInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setSignupState({
+      ...signupState,
+      [name]: value,
+    });
+  };
+
   return (
-    <>
-      <SignUpPage>
-        <SignUpWrapper>
-          <SignUpForm>
+    <SignUpPage>
+      <SignUpWrapper>
+        <SignUpForm>
+          <b>회원가입</b>
+        </SignUpForm>
+        <SignUpBox>
+          <NicknameForm>
+            <b>닉네임</b>
+          </NicknameForm>
+          <SignUpInput
+            name="nickname"
+            placeholder="닉네임을 입력해주세요"
+            onChange={SignUpInputChange}
+            maxLength={10}
+          />
+          <EmailForm>
+            <b>이메일</b>
+          </EmailForm>
+          <SignUpInput
+            onChange={SignUpInputChange}
+            name="email"
+            placeholder="이메일을 입력해주세요"
+          />
+          <PwForm>
+            <b>비밀번호</b>
+          </PwForm>
+          <SignUpInput
+            onChange={SignUpInputChange}
+            name="password"
+            placeholder="비밀번호를 입력해주세요"
+            minLength={8}
+            maxLength={20}
+          />
+          <SignUpBtn>
             <b>회원가입</b>
-          </SignUpForm>
-          <SignUpBox>
-            <NicknameForm>
-              <b>닉네임</b>
-            </NicknameForm>
-            <Nickname placeholder="아이디를 입력해주세요" />
-            <EmailForm>
-              <b>이메일</b>
-            </EmailForm>
-            <Email placeholder="이메일을 입력해주세요" />
-            <PwForm>
-              <b>비밀번호</b>
-            </PwForm>
-            <PassWord placeholder="비밀번호를 입력해주세요" />
-            <SignUpBtn>
-              <b>회원가입</b>
-            </SignUpBtn>
-          </SignUpBox>
-        </SignUpWrapper>
-      </SignUpPage>
-    </>
+          </SignUpBtn>
+        </SignUpBox>
+      </SignUpWrapper>
+    </SignUpPage>
   );
 };
 
@@ -64,22 +100,13 @@ const NicknameForm = styled.div`
   font-size: 20px;
   margin-bottom: 30px;
 `;
-const Nickname = styled.input`
-  border: none;
-  border-bottom: 3px ${theme.mainBlueColor} solid;
-  display: flex;
-  width: 420px;
-  padding-bottom: 10px;
-  outline: none;
-  font-size: 16px;
-  margin-bottom: 42px;
-`;
+
 const EmailForm = styled.div`
   color: ${theme.mainBlueColor};
   font-size: 20px;
   margin-bottom: 30px;
 `;
-const Email = styled.input`
+const SignUpInput = styled.input`
   border: none;
   border-bottom: 3px ${theme.mainBlueColor} solid;
   display: flex;
@@ -94,17 +121,9 @@ const PwForm = styled.div`
   font-size: 20px;
   margin-bottom: 30px;
 `;
-const PassWord = styled.input`
-  border: none;
-  border-bottom: 3px ${theme.mainBlueColor} solid;
-  display: flex;
-  width: 420px;
-  padding-bottom: 10px;
-  outline: none;
-  font-size: 16px;
-  margin-bottom: 76px;
-`;
+
 const SignUpBtn = styled.button`
+  cursor: pointer;
   width: 420px;
   height: 60px;
   border-radius: 33px;
@@ -119,4 +138,10 @@ const SignUpBtn = styled.button`
     rgba(0, 100, 255, 0.4),
     ${theme.mainBlueColor}
   );
+
+  :hover {
+    border: 1px solid ${theme.mainBlueColor};
+    background: white;
+    color: ${theme.mainBlueColor};
+  }
 `;
