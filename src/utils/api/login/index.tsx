5714fa2b2a4f3e976f56d668e7b781b41873b4dc
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-12-25 22:07:34 +0900

    UPDATE: BASE_URL env

--- src/utils/api/login/index.tsx
+++ src/utils/api/login/index.tsx
@@ -3,12 +3,11 @@ import { setToken } from 'utils/functions/token/tokenManager';
 import { LoginType } from 'utils/interface/login';
 import { tokenType } from 'utils/interface/token';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 export const postLogin = async (loginData: LoginType) => {
   try {
-    const { data } = await axios.post(
-      'http://3.38.77.146:8080/user/token',
-      loginData,
-    );
+    const { data } = await axios.post(`${BASE_URL}/user/token`, loginData);
     const { access_token, refresh_token }: tokenType = data;
 
     setToken(access_token, refresh_token);
