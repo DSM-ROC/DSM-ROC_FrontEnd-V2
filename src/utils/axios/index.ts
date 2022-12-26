d41886c9d438d6c8e83caa10a59b92cf213eff89
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-12-26 17:48:45 +0900

    UPDATE: instance

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -34,37 +34,8 @@ instance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    const navigate = useNavigate();
-
     if (axios.isAxiosError(error) && error.response) {
-      const { config, response } = error;
-      if (response.data.status === 401 && getToken().refreshToken) {
-        try {
-          const res = await axios({
-            method: 'patch',
-            url: `${process.env.REACT_APP_BUBLIC_URL}/users/token`,
-            headers: {
-              'Refresh-Token': `Bearer ${getToken().refreshToken}`,
-            },
-          });
-
-          const { access_token, refresh_token } = res.data;
-
-          setToken(access_token, refresh_token);
-          if (config && config.headers)
-            config.headers.Authorization = `Bearer ${access_token}`;
-          if (config) return axios(config);
-        } catch (err: any) {
-          if (
-            err.response.data.status === 401 ||
-            err.response.data.status === 404
-          ) {
-            ToastError('다시 로그인해주세요.');
-            navigate('/login');
-            removeToken();
-          }
-        }
-      } else return Promise.reject(error);
+      return Promise.reject(error);
     }
   },
 );
